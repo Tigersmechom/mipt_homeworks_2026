30a6bdf85e6dfaad3fa17bbcec7c94b9e2f4383c
123 <danilnewtryhard@gmail.com>
2026-03-31 20:14:49 +0300

    hw3

--- part3_types_conditions_loops_functions/hw3.py
+++ part3_types_conditions_loops_functions/hw3.py
@@ -68,7 +68,11 @@ def is_leap_year(year: int) -> bool:
     :return: Значение високосности.
     :rtype: bool
     """
-    return year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    return year % 4 == 0
 
 
 def split_date_parts(maybe_dt: str) -> tuple[str, str, str] | None:
